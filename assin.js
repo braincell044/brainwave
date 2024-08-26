b5f3bfdb01ce4b71e8266158931553e13f04511e
USER <user@USERs-MacBook-Pro.local>
2024-08-26 22:46:18 +0100

    done now

--- assin.js
+++ assin.js
@@ -76,19 +76,16 @@ else{
 (5)Generate a random number between 50 and 100 inclusively done*/
 
 //(1) Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
-let num_2 = 9.81
-let numint = parseInt(num_2)
-console.log(numint)
 let myParseFloat = 9.8
 if (myParseFloat != 10){
-    console.log(Math.round(myParseFloat))
+    console.log("parseFloate:" , Math.round(myParseFloat))
 }
 //(2)Check if 'on' is found in both python and jargon
 let course = ['python','jargon']
 const containOn = course.includes('on')
 if (containOn === true){console.log('this course is jargon')}
 else{
-    console.log('no be jargon')
+    console.log('these is not jargon')
 }
 //(3)I hope this course is not full of jargon. Check if jargon is in the sentence.
 
@@ -98,16 +95,16 @@ if (containjargon === true){
     console.log("these is jargon")
 }
 else{
-    console.log("no be jargon")
+    console.log("these is not jargon")
 }
 
 //Generate a random number between 0 and 100 inclusively. 
 
 let random1 = Math.random()*101
-console.log(random1)
+console.log("these is random num 0-100:",random1)
 //Generate a random number between 50 and 100 inclusively
-let random2 =  Math.random() * (100 - 50 + 1)
-console.log(random2)
+let random2 =  Math.random() *  (100 - 50 +1)+(50)
+console.log("these is random num 50-100:",random2)
 
 /*const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 (1) Sort the array and find the min and max age  done
@@ -122,8 +119,8 @@ const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 //(1) Sort the array and find the min and max age
 let min = Math.min(... ages)
 let max = Math.max(...ages)
-console.log(min)
-console.log(max)
+console.log("min:",min)
+console.log("max:",max)
 
 
 //(2) Find the median age(one middle item or two middle items divided by two)
@@ -133,7 +130,7 @@ if (ages.length % 2 === 0){
     const mid1 = ages[ages.length / 2 - 1];
     const mid2 = ages[ages.length / 2 ]
     median = (mid1+ mid2) / 2;
-    console.log(median)
+    console.log("median ages:",median)
 
 
 }else{
@@ -145,17 +142,17 @@ let totalAge = ages.reduce((sum, age) => sum + age, 0)
 let average = totalAge / ages.length
 if (ages.length % 2 === 0){
    average / ages.length
-    console.log(average)
+    console.log("average:",average)
 }
 //(4)Find the range of the ages(max minus min)
 let range = max - min
-console.log(range)
+console.log("range:",range)
 
 //(5) Compare the value of (min - average) and (max - average), use abs() method.
 let  abs1 = Math.abs(min - average) 
-console.log(abs1)
+console.log("abs1:",abs1)
 let abs2 = Math.abs(max - average)
-console.log(abs2)
+console.log("abs2:",abs2)
 
 /* 
  Write a code which can give grades to students according to theirs scores:
